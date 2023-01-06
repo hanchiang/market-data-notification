fe7c18d97d04c162f3474a054e596ff3a72da4db
han chiang <yaphc@hotmail.com>
2023-01-06 21:56:57 +0800

    Save tradingview data to redis

--- src/util/my_telegram.py
+++ src/util/my_telegram.py
@@ -2,3 +2,7 @@ import telegram
 
 def escape_markdown(text: str, version=2):
     return telegram.helpers.escape_markdown(text=text, version=version)
+
+def format_messages_to_telegram(messages: list[str]) -> str:
+    return escape_markdown("\n-----------------------------------------------------------------\n").join(
+        messages)

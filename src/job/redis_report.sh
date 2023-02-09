db69b2e92512a3a6e54fc0245483099de355252a
han chiang <yaphc@hotmail.com>
2023-02-09 21:35:15 +0800

    Update sendgrid template id variable

--- src/job/redis_report.sh
+++ src/job/redis_report.sh
@@ -4,7 +4,7 @@ SENDGRID_API_KEY=$1
 EMAIL_RECIPIENT=$2
 EMAIL_SENDER=$3
 REDIS_KEY=$4
-TEMPLATE_ID=$5
+SENDGRID_TEMPLATE_ID=$5
 
 if [ -z "$SENDGRID_API_KEY"  ];
 then
@@ -40,7 +40,7 @@ redis_data=$(echo $redis_data | sed -e "s/\"/'/g" )
 
 # TODO: write in python
 maildata='{"personalizations": [{"to": [{"email": "'${EMAIL_RECIPIENT}'"}], "dynamic_template_data": { "redis_data": "'${redis_data}'" } }],"from": {"email": "'${EMAIL_SENDER}'",
-	"name": "'${FROM_NAME}'"},"subject": "'${SUBJECT}'", "template_id": "'${TEMPLATE_ID}'"}'
+	"name": "'${FROM_NAME}'"},"subject": "'${SUBJECT}'", "template_id": "'${SENDGRID_TEMPLATE_ID}'"}'
 
 curl --request POST \
   --url https://api.sendgrid.com/v3/mail/send \

0c4d4f0622eb9f752837511aed60e8276b01fa89
Han Chiang <yaphc@hotmail.com>
2026-03-25 23:11:43 +0800

    Make cron runner compatible with old and new images

--- scripts/cron/run_notification_job.sh
+++ scripts/cron/run_notification_job.sh
@@ -16,6 +16,13 @@ JOB_PATH=$2
 FORCE_RUN=${3:-0}
 TEST_MODE=${4:-0}
 
+RUNTIME_COMMAND='python'
+if docker exec -i -w /app "$CONTAINER_NAME" test -x /app/.venv/bin/python; then
+    RUNTIME_COMMAND='/app/.venv/bin/python'
+elif docker exec -i -w /app "$CONTAINER_NAME" test -x /root/.local/bin/poetry; then
+    RUNTIME_COMMAND='/root/.local/bin/poetry run python'
+fi
+
 # Run inside the existing app container so cron uses the deployed application and dependency set.
-docker exec -i -w /app "$CONTAINER_NAME" python \
-    "$JOB_PATH" --force_run "$FORCE_RUN" --test_mode "$TEST_MODE"
+docker exec -i -w /app "$CONTAINER_NAME" sh -c \
+    "$RUNTIME_COMMAND \"$JOB_PATH\" --force_run \"$FORCE_RUN\" --test_mode \"$TEST_MODE\""

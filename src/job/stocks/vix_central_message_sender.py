7b84def5528517e73d24490c98e8e3f3d0030045
Han Chiang <yaphc@hotmail.com>
2026-04-04 23:05:51 +0800

    Fix VIX Central contract roll alerts

--- src/job/stocks/vix_central_message_sender.py
+++ src/job/stocks/vix_central_message_sender.py
@@ -42,7 +42,6 @@ class VixCentralMessageSender(MessageSenderWrapper):
 
     def _format_vix_futures_values(self, res, curr):
         message = f"{res}\ndate: {escape_markdown(curr.current_date)}, contango %: {escape_markdown(curr.formatted_contango)}"
-        message = f"{message}" if curr.formatted_contango_change_prev_day is None else f"{message}"
         if curr.formatted_contango_change_prev_day is not None:
             message = f"{message}, changed by {escape_markdown(curr.formatted_contango_change_prev_day)} from the previous day"
         if curr.is_contango_single_day_decrease_alert:
